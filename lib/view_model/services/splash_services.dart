5d8baadc8f3d849b9ec8eee6e70faa49a898d6d5
Muhammad Usman <ahmadkhan75830@gmail.com>
2025-01-29 02:00:54 +0500

    User Session

--- lib/view_model/services/splash_services.dart
+++ lib/view_model/services/splash_services.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:rental_sphere/res/components/navigation_helper.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../utils/routes/routes_name.dart';
 
 class SplashServices {
 
 
-  void checkAuthentication(BuildContext context) async{
+  void checkAuthentication(BuildContext context) async {
+    SharedPreferences sp = await SharedPreferences.getInstance();
+    String? role = sp.getString('role') ?? '';
 
-
-        await Future.delayed(const Duration(seconds: 3));
-       NavigationHelper.navigateWithSlideTransition(context: context, routeName: RoutesName.signUp, replace: true).onError((error, stackTrace){
-      if(kDebugMode){
-        print(error.toString());
-      }
-    });
+    await Future.delayed(const Duration(seconds: 3));
+    if (role == '') {
+      NavigationHelper.navigateWithSlideTransition(
+          context: context, routeName: RoutesName.signUp, replace: true);
+    }
+    else if (role != '') {
+      NavigationHelper.navigateWithSlideTransition(
+          context: context, routeName: RoutesName.home, replace: true)
+          .onError((error, stackTrace) {
+        if (kDebugMode) {
+          print(error.toString());
+        }
+      });
+    }
   }
 }
